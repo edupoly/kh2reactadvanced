47d60307b744c46df5d4f438e506f2810255ade5
edupoly <info.edupoly@gmail.com>
2022-09-13 19:26:09 +0530

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Countries from './Countries';
+import { Link, Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='border m-2 p-2'>
+      <Link to="/home">Home</Link>&nbsp;&nbsp;&nbsp;
+      <Link to="/countries">Countries</Link>&nbsp;&nbsp;&nbsp;
+      <Outlet></Outlet>
     </div>
   );
 }
---
+++ src/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1>Home Page</h1>
+        <p>
+        n previous versions of React Router you had to order your routes a certain way to get the right one to render when multiple routes matched an ambiguous URL. V6 is a lot smarter and will pick the most specific match so you don't have to worry about that anymore. For example, the URL /teams/new matches both of these route:
+        </p>
+        <h1>Home Page</h1>
+        <p>
+        n previous versions of React Router you had to order your routes a certain way to get the right one to render when multiple routes matched an ambiguous URL. V6 is a lot smarter and will pick the most specific match so you don't have to worry about that anymore. For example, the URL /teams/new matches both of these route:
+        </p>
+        <h1>Home Page</h1>
+        <p>
+        n previous versions of React Router you had to order your routes a certain way to get the right one to render when multiple routes matched an ambiguous URL. V6 is a lot smarter and will pick the most specific match so you don't have to worry about that anymore. For example, the URL /teams/new matches both of these route:
+        </p>
+        <h1>Home Page</h1>
+        <p>
+        n previous versions of React Router you had to order your routes a certain way to get the right one to render when multiple routes matched an ambiguous URL. V6 is a lot smarter and will pick the most specific match so you don't have to worry about that anymore. For example, the URL /teams/new matches both of these route:
+        </p>
+    </div>
+  )
+}
+
+export default Home
