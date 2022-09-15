0d97a3e4ad1bda662eb692edc1dea6667621fcf4
edupoly <info.edupoly@gmail.com>
2022-09-15 20:26:59 +0530

    RESET

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -8,11 +8,15 @@ function Counter(props) {
     function dec(){
         props.dispatch({type:"DECREMENT"})
     }
+    function reset(){
+        props.dispatch({type:'RESET'})
+    }
     return (
         <div className='betterview'>
             <h1>Counter:{props.count}</h1>
             <button onClick={inc}>Increment</button>
             <button onClick={dec}>Decrement</button>
+            <button onClick={reset}>RESET</button>
         </div>
     )
 }
--- src/store/counterReducer.js
+++ src/store/counterReducer.js
@@ -8,6 +8,9 @@ function counterReducer(state=initialState,action){
     if(action.type==='DECREMENT'){
         return {...state,count:state.count-1}
     }
+    if(action.type==='RESET'){
+        return {...state,count:initialState.count}
+    }
     return state
 }
 
