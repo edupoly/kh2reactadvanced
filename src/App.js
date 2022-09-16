36f6e8491e68d7afeaf71ca658df1967aa6ad882
edupoly <info.edupoly@gmail.com>
2022-09-16 20:50:07 +0530

    shopping cart

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import mainstore from './store/store';
 import { Provider } from 'react-redux';
-import Counter from './Counter';
-import Todolist from './Todolist';
+import Counter from './components/Counter';
+import Todolist from './components/Todolist';
+import Header from './components/Header';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <Provider store={mainstore}>
-      <div className="betterview">
-        <h1>App Component</h1>
-        <Todolist></Todolist>
-        <Counter></Counter>
+      <div>
+        <Header></Header>
+        <Outlet></Outlet>
+        
       </div>
     </Provider>
   );
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
 import counterReducer from "./counterReducer";
 import { combineReducers, createStore } from "redux";
 import todoReaducer from "./todoReducer";
-var reducer = combineReducers({counter:counterReducer,todolist:todoReaducer})
+import productReducer from "./productReducer";
+var reducer = combineReducers({counter:counterReducer,todolist:todoReaducer,products:productReducer})
 var mainstore = new createStore(reducer)
 
 export default mainstore;
