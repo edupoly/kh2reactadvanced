78ac02e6043465653ca22eba3848c8bc81c105ef
edupoly <info.edupoly@gmail.com>
2022-09-16 21:17:00 +0530

    cart table

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -10,19 +10,39 @@ function Cart({products:{cart},dispatch}) {
         dispatch({type:'DEC-CART-PROD-COUNT',payload:cp})
     }
   return (
-    <div>
-        <h1>Cart</h1>
-        {
-            cart.map((cp,i)=>{
-                return(<li>
-                    {cp.title}---{cp.price}
-                    <button onClick={()=>{dispatch({type:'INC-CART-PROD-COUNT',payload:cp})}}>+</button>
-                    <b>{cp.count}</b>
-                    <button onClick={()=>{dec(cp)}}>-</button>
-                    <button onClick={()=>{dispatch({type:'DELETE-CART-PROD',payload:cp})}}>Delete</button>
-                </li>)
-            })
-        }
+    <div className='container'>
+        <table className="table table-striped">
+            <thead>
+            <tr>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Count</th>
+                <th>Total</th>
+                <th>Delete</th>
+            </tr>
+            </thead>
+            <tbody>
+            {
+                cart.map((cp,i)=>{
+                    return(<tr>
+                        <td>
+                            {cp.title}</td>
+                        <td>{cp.price}</td>
+                        <td>
+                            <button className='btn btn-success' onClick={()=>{dispatch({type:'INC-CART-PROD-COUNT',payload:cp})}}>+</button>
+                            <b>{cp.count}</b>
+                            <button className='btn btn-danger' onClick={()=>{dec(cp)}}>-</button>
+                        </td>
+                        <td>{cp.price*cp.count}</td>
+                        <td>
+                        <button onClick={()=>{dispatch({type:'DELETE-CART-PROD',payload:cp})}}>Delete</button>
+                        </td>
+                    </tr>)
+                })
+            }
+            </tbody>
+        </table>
+        
         <h1>Total:</h1>
     </div>
   )
